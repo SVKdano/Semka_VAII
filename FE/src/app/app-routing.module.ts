684b82601c0a24f4bfe32d0df33c4baae0bf22fd
DanielLieskovsky <danol147@gmail.com>
2023-01-16 22:00:53 +0100

    routing for each league done

--- FE/src/app/app-routing.module.ts
+++ FE/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import {WelcomeComponent} from "./welcome/welcome.component";
+import {LigaPatComponent} from "./liga-pat/liga-pat.component";
+import {LigaSestComponent} from "./liga-sest/liga-sest.component";
+import {LigaSedemComponent} from "./liga-sedem/liga-sedem.component";
 
 const routes: Routes = [
-  { path: "", component: WelcomeComponent }
+  { path: "", component: WelcomeComponent },
+  { path: "leagueFive", component: LigaPatComponent},
+  { path: "leagueSix", component: LigaSestComponent},
+  { path: "leagueSeven", component: LigaSedemComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
