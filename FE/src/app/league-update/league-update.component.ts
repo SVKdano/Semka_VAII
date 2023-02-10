3f6042596ca0a30eccec1ba188c98d2955bebea3
DanielLieskovsky <danol147@gmail.com>
2023-02-10 12:48:09 +0100

    Adjusted: CRUD operations of leagues done

--- FE/src/app/league-update/league-update.component.ts
+++ FE/src/app/league-update/league-update.component.ts
@@ -12,13 +12,15 @@ export class LeagueUpdateComponent implements OnInit {
   @Input() league: League = {id:0, name:""};
   leagueId: number = 0;
   leagueName: string = "";
+
   leagues: League[] = [];
+
   @Output() update = new EventEmitter<League[]>();
+
 
   constructor(private router:Router, private service:TeamsServiceService) {}
 
   ngOnInit() {
-    this.service.getAllLeagues().subscribe((result : League[]) => (this.leagues = result));
   }
 
   initializeUpdating(id: number, name: string) {
@@ -29,17 +31,39 @@ export class LeagueUpdateComponent implements OnInit {
   updateLeague() {
     this.league.id = this.leagueId;
     this.league.name = this.leagueName;
-    this.service.updateLeague(this.league).subscribe((result:League[]) => this.update.emit(result));
+    this.service.updateLeague(this.league).subscribe(
+      (result:League[]) =>
+      {
+        this.update.emit(result);
+        this.fetchData();
+      }
+    );
   }
 
   deleteLeague(id: number) {
-    this.service.deleteLeague(id).subscribe((result:League[]) => this.update.emit(result));
+    this.service.deleteLeague(id).subscribe(
+      (result:League[]) => {
+        this.update.emit(result);
+        this.fetchData();
+      }
+   );
   }
 
   createLeague() {
     this.league.id = this.leagueId;
     this.league.name = this.leagueName;
-    this.service.createLeague(this.league).subscribe((result:League[]) => this.update.emit(result));
+    this.service.createLeague(this.league).subscribe(
+      (result:League[]) => {
+        this.update.emit(result);
+        this.fetchData();
+      }
+  );
+  }
+
+  fetchData() {
+    this.service.getAllLeagues().subscribe((result : League[]) => (this.leagues = result));
+    //TODO: wait for message from P
+    setTimeout(() => { this.fetchData() }, 2000);
   }
 
   public logout() {
